9e09660770141eb4a84f8e1414dfeaed09ff5389
WisleyAraujo <wisleyaraujosanstos@gmail.com>
2023-04-27 12:27:24 -0300

    launch.json

--- projeto/settings.py
+++ projeto/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/``
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asdasdasdasdadasdasdasda'
+SECRET_KEY = 'wisley'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
