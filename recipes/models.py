13d7fc139809fc646ff3a1885f4a6e2a55a3691d
WisleyAraujo <wisleyaraujosanstos@gmail.com>
2023-05-11 10:49:02 -0300

    criando minha primeira migration, e adicionando novos estilos

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.h
+# Create your models here 
+# cada model, representa uma tabela no banco de dados.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug  = models.SlugField() # slug indexado
+    prepararion_time = models.IntegerField()
+    prepararion_time_unit = models.CharField(max_length=65)
+    prepararion_steps = models.TextField()
+    prepararion_steps_is_html = models.BooleanField(default=False)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    created_at = models.DateTimeField(auto_now_add=True) #gera uma data no momento da criacao e nao mexe mais nela 
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%d/%m/%Y/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
